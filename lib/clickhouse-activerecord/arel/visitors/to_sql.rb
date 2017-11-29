f9a4bb37056ab4af8614f4ff7705edd0a05c26bd
nixx <nixx.dj@gmail.com>
2017-11-29 18:12:37 +0300

    fix arel relation

--- lib/clickhouse-activerecord/arel/visitors/to_sql.rb
+++ lib/clickhouse-activerecord/arel/visitors/to_sql.rb
@@ -7,7 +7,7 @@ module ClickhouseActiverecord
 
         def aggregate(name, o, collector)
           # replacing function name for materialized view
-          if o.expressions.first.relation.engine.is_view
+          if o.expressions.first && o.expressions.first.relation && o.expressions.first.relation.engine && o.expressions.first.relation.engine.is_view
             super("#{name.downcase}Merge", o, collector)
           else
             super
