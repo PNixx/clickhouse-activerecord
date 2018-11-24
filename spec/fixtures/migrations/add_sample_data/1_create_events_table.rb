0fc1bc3d129f21c6b6c41d1d7b070ff9a4f82f0a
Kirill Zaitsev <kirik910@gmail.com>
2018-11-25 00:11:42 +0200

    Simple models tests

---
+++ spec/fixtures/migrations/add_sample_data/1_create_events_table.rb
@@ -0,0 +1,10 @@
+
+class CreateEventsTable < ActiveRecord::Migration[5.0]
+  def up
+    create_table :events, options: 'MergeTree(date, (date, event_name), 8192)' do |t|
+      t.string :event_name
+      t.date :date
+    end
+  end
+end
+
