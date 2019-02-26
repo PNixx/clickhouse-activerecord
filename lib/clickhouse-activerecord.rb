de43b8241a59ab746fc27cf466a594848940470f
nixx <nixx.dj@gmail.com>
2019-02-26 23:50:39 +0300

    database create / drop, migration support with rake tasks

--- lib/clickhouse-activerecord.rb
+++ lib/clickhouse-activerecord.rb
@@ -5,6 +5,8 @@ require 'active_record/connection_adapters/clickhouse_adapter'
 if defined?(Rails::Railtie)
   require 'clickhouse-activerecord/railtie'
   require 'clickhouse-activerecord/schema_dumper'
+  require 'clickhouse-activerecord/tasks'
+  ActiveRecord::Tasks::DatabaseTasks.register_task(/clickhouse/, "ClickhouseActiverecord::Tasks")
 end
 
 module ClickhouseActiverecord
