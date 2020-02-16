9725e0e400fac11a911927d6767a6ead6fabe83d
astudnev <astudnev@gmail.com>
2020-02-16 15:42:17 +0300

    limit grouping to 1 column only uniq

--- lib/clickhouse-activerecord/arel/select_manager.rb
+++ lib/clickhouse-activerecord/arel/select_manager.rb
@@ -2,8 +2,30 @@ module ClickhouseActiverecord::Arel
 
     class SelectManager < ::Arel::SelectManager
 
+      def initialize(table = nil)
+        super
+        @columns_grouped = []
+      end
+
       def using(*exprs)
         @ctx.source.right.last.right = ::ClickhouseActiverecord::Arel::Nodes::Using.new(::Arel.sql(exprs.join(',')))
+        self
+      end
+
+      def group(*columns)
+        columns.each do |column|
+
+          unless @columns_grouped.include? column
+            @columns_grouped.push column
+
+            column = ::Arel::Nodes::SqlLiteral.new(column) if String === column
+            column = ::Arel::Nodes::SqlLiteral.new(column.to_s) if Symbol === column
+
+            @ctx.groups.push ::Arel::Nodes::Group.new column
+
+          end
+
+        end
         self
       end
 
