ef1f1e9a33a1ac36531ee08d4d91e565f511a101
Ryan Kerr <leboshi@gmail.com>
2022-02-18 01:45:53 -0500

    Replace Migrator and CommandRecorder injections

--- lib/clickhouse-activerecord.rb
+++ lib/clickhouse-activerecord.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require_relative '../core_extensions/active_record/migration/command_recorder'
+
 require 'active_record/connection_adapters/clickhouse_adapter'
 
 require 'clickhouse-activerecord/base'
 require 'clickhouse-activerecord/internal_metadata'
+require 'clickhouse-activerecord/migrator'
 require 'clickhouse-activerecord/relation'
 require 'clickhouse-activerecord/schema_migration'
 require 'clickhouse-activerecord/type_caster/map'
@@ -21,6 +24,8 @@ module ClickhouseActiverecord
   def self.load
     ActiveRecord::Base.singleton_class.prepend(ClickhouseActiverecord::Base::ClassMethods)
     ActiveRecord::InternalMetadata.singleton_class.prepend(ClickhouseActiverecord::InternalMetadata::ClassMethods)
+    ActiveRecord::Migration::CommandRecorder.include CoreExtensions::ActiveRecord::Migration::CommandRecorder
+    ActiveRecord::Migrator.prepend(ClickhouseActiverecord::Migrator)
     ActiveRecord::Relation.prepend(ClickhouseActiverecord::Relation)
     ActiveRecord::SchemaMigration.singleton_class.prepend(ClickhouseActiverecord::SchemaMigration::ClassMethods)
     ActiveRecord::TypeCaster::Map.prepend(ClickhouseActiverecord::TypeCaster::Map)
---
+++ lib/clickhouse-activerecord/migrator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module ClickhouseActiverecord
+  module Migrator
+    def record_version_state_after_migrating(version)
+      if down?
+        migrated.delete(version)
+        @schema_migration.create!(version: version.to_s, active: 0)
+      else
+        super
+      end
+    end
+  end
+end
