19ae5c3af012d53acc58153bb6b64096742f446f
gjnelson112 <gjnelson112@gmail.com>
2022-04-05 14:34:39 -0400

    add guard clause to 'record_version_state_after_migrating' so this logic only runs if a clickhouse migration; currently this method gets run every time we rollback any migraiton, even if not clickhouse related which leads to this error "unknown attribute 'active' for ActiveRecord::SchemaMigration."
    Co-authored-by: William Dana <william.b.dana@gmail.com>

--- lib/core_extensions/active_record/migrator.rb
+++ lib/core_extensions/active_record/migrator.rb
@@ -4,6 +4,8 @@ module CoreExtensions
   module ActiveRecord
     module Migrator
       def record_version_state_after_migrating(version)
+        return super unless @schema_migration.connection.adapter_name == "Clickhouse"
+
         if down?
           migrated.delete(version)
           @schema_migration.create!(version: version.to_s, active: 0)
