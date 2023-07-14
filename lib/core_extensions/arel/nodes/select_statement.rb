49c06430051bcbb48af3fc86c9a8a7de118d50fb
Ryan Kerr <leboshi@gmail.com>
2023-07-13 20:25:19 -0400

    Fix SelectStatement injection to work with both Rails 6 and 7.
    Stupid Arel::Nodes::SelectStatement constructor changed its signature...

--- lib/core_extensions/arel/nodes/select_statement.rb
+++ lib/core_extensions/arel/nodes/select_statement.rb
@@ -6,9 +6,13 @@ module CoreExtensions
       module SelectStatement
         attr_accessor :settings
 
-        def initialize(relation = nil)
-          super
+        def initialize(cores = [::Arel::Nodes::SelectCore.new])
           @settings = nil
+
+          return super if ::ActiveRecord.version < Gem::Version.new('7')
+
+          relation = (cores unless cores.is_a?(Array) && cores.all? { |c| c.is_a?(::Arel::Nodes::SelectCore) })
+          super(relation)
         end
 
         def hash
