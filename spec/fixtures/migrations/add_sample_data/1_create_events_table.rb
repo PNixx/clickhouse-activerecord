191ac67ed5eba2e1cbd20306d855b85b603e54d4
nixx <nixx.dj@gmail.com>
2023-10-13 11:04:45 +0300

    convert aggregation type

--- spec/fixtures/migrations/add_sample_data/1_create_events_table.rb
+++ spec/fixtures/migrations/add_sample_data/1_create_events_table.rb
@@ -4,6 +4,7 @@ class CreateEventsTable < ActiveRecord::Migration[5.0]
   def up
     create_table :events, options: 'MergeTree(date, (date, event_name), 8192)' do |t|
       t.string :event_name, null: false
+      t.integer :event_value
       t.date :date, null: false
     end
   end
