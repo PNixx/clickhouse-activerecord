a3c6e6fead16a7ae30c775c5c8c9c7c3a38e077f
nixx <nixx.dj@gmail.com>
2024-08-16 15:46:18 +0300

    fix dump schema with numeric default

--- spec/fixtures/migrations/dsl_table_with_decimal_creation/1_create_some_table.rb
+++ spec/fixtures/migrations/dsl_table_with_decimal_creation/1_create_some_table.rb
@@ -5,6 +5,7 @@ class CreateSomeTable < ActiveRecord::Migration[7.1]
     create_table :some do |t|
       t.decimal :money, precision: 16, scale: 4
       t.decimal :balance, precision: 32, scale: 2, null: false, default: 0
+      t.decimal :paid, precision: 32, scale: 2, null: false, default: 1.15
     end
   end
 end
--- spec/fixtures/migrations/dsl_table_with_enum_creation/1_create_some_table.rb
+++ spec/fixtures/migrations/dsl_table_with_enum_creation/1_create_some_table.rb
@@ -3,7 +3,7 @@
 class CreateSomeTable < ActiveRecord::Migration[7.1]
   def up
     create_table :some, id: false do |t|
-      t.enum :enum8, value: { key1: 1, key2: 2 }, limit: 1, null: false
+      t.enum :enum8, value: { key1: 1, key2: 2 }, limit: 1, null: false, default: :key1
       t.enum :enum16, value: { key1: 1, key2: 2 }, limit: 2, null: false
       t.enum :enum_nullable, value: { key1: 1, key2: 2 }, null: true
     end
