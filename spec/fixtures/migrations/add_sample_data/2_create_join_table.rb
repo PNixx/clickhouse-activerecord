e1966e79d996daca57e4a73b12dbe84f7d3c4cda
nixx <nixx.dj@gmail.com>
2024-08-27 15:17:30 +0300

    fix detect model primary key

--- spec/fixtures/migrations/add_sample_data/2_create_join_table.rb
+++ spec/fixtures/migrations/add_sample_data/2_create_join_table.rb
@@ -2,7 +2,7 @@
 
 class CreateJoinTable < ActiveRecord::Migration[7.1]
   def up
-    create_table :joins, options: 'MergeTree PARTITION BY toYYYYMM(date) ORDER BY (event_name)' do |t|
+    create_table :joins, id: false, options: 'MergeTree PARTITION BY toYYYYMM(date) ORDER BY (event_name)' do |t|
       t.string :event_name, null: false
       t.integer :event_value
       t.integer :join_value
