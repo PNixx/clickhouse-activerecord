33652bf08135b7fe6d8eecb557f8369b774916fc
Daniel Westendorf <daniel@prowestech.com>
2024-10-23 06:38:51 -0400

    Include column definitions in schema dump if the column name is not `id` (#173)

--- lib/clickhouse-activerecord/schema_dumper.rb
+++ lib/clickhouse-activerecord/schema_dumper.rb
@@ -78,7 +78,7 @@ module ClickhouseActiverecord
           if simple || !match
             columns.each do |column|
               raise StandardError, "Unknown type '#{column.sql_type}' for column '#{column.name}'" unless @connection.valid_type?(column.type)
-              next if column.name == pk
+              next if column.name == pk && column.name == "id"
               type, colspec = column_spec(column)
               name = column.name =~ (/\./) ? "\"`#{column.name}`\"" : column.name.inspect
               tbl.print "    t.#{type} #{name}"
