5b63b6b0364196427ef06d9830bc7d8efc054e57
nixx <nixx.dj@gmail.com>
2026-01-19 14:40:18 +0300

    fix in the `with` clause for ar 7.1

--- lib/core_extensions/active_record/relation/query_methods.rb
+++ lib/core_extensions/active_record/relation/query_methods.rb
@@ -12,10 +12,27 @@ module CoreExtensions
         self
       end
 
+      def build_with_value_from_hash(hash)
+        return super if ::ActiveRecord::version >= Gem::Version.new('7.2')
+
+        # Redefine for ActiveRecord < 7.2
+        hash.map do |name, value|
+          expression =
+            case value
+            when ::Arel::Nodes::SqlLiteral then ::Arel::Nodes::Grouping.new(value)
+            when ::ActiveRecord::Relation then value.arel
+            when ::Arel::SelectManager then value
+            when Symbol then value
+            else
+              raise ArgumentError, "Unsupported argument type: `#{value}` #{value.class}"
+            end
+          ::Arel::Nodes::TableAlias.new(expression, name)
+        end
+      end
+
       def build_with_expression_from_value(value, nested = false)
         case value
-        when Symbol
-          value
+        when Symbol then value
         else
           super
         end
