175c7203ec9303caf87e4e419682a304527587f7
Nate Vick <nate@vick.codes>
2026-01-20 21:27:30 -0800

    Fix test helpers to restore original database connection (#229)
    The minitest and rspec helpers were changing ActiveRecord::Base.connection
    to ClickHouse without restoring it afterward. This broke multi-database
    Rails apps where the primary database is PostgreSQL/MySQL.

--- lib/clickhouse-activerecord/minitest.rb
+++ lib/clickhouse-activerecord/minitest.rb
@@ -4,10 +4,13 @@ module ClickhouseActiverecord
   module TestHelper
     def before_setup
       super
+      original_connection_config = ActiveRecord::Base.connection_db_config
       ActiveRecord::Base.configurations.configurations.select { |x| x.env_name == Rails.env && x.adapter == 'clickhouse' }.each do |config|
         ActiveRecord::Base.establish_connection(config)
         ActiveRecord::Base.connection.truncate_tables(*ActiveRecord::Base.connection.tables)
       end
+    ensure
+      ActiveRecord::Base.establish_connection(original_connection_config) if original_connection_config
     end
   end
 end
