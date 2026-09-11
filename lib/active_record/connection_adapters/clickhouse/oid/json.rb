c9a22533c174defcd5ff5615a98f0e288141e7a4
nixx <nixx.dj@gmail.com>
2026-09-11 10:14:55 +0300

    refactoring

--- lib/active_record/connection_adapters/clickhouse/oid/json.rb
+++ lib/active_record/connection_adapters/clickhouse/oid/json.rb
@@ -4,38 +4,18 @@ module ActiveRecord
   module ConnectionAdapters
     module Clickhouse
       module OID # :nodoc:
-        class Json < Type::Value # :nodoc:
-
-          def type
-            :json
-          end
+        class Json < Type::Json # :nodoc:
 
           def deserialize(value)
-            return value if value.nil?
-            return value if value.is_a?(::Hash) || value.is_a?(::Array)
-
             if value.is_a?(::String)
               ::JSON.parse(value)
             else
               super
             end
-          rescue ::JSON::ParserError
+          rescue ::JSON::ParserError => e
+            ActiveSupport.error_reporter.report(e, source: "application.active_record")
             nil
           end
-
-          def serialize(value)
-            return nil if value.nil?
-            ::JSON.generate(value)
-          end
-
-          def changed_in_place?(raw_old_value, new_value)
-            deserialize(raw_old_value) != new_value
-          end
-
-          def accessor
-            ActiveRecord::Store::StringKeyedHashAccessor
-          end
-
         end
       end
     end
